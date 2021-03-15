dc396c4d5185fcab5a5cde05fd7e65ed1dbdca6b
Pascaline_T <pascaline.thuilliez21@gmail.com>
2021-03-15 18:41:37 +0100

    exo algo du 15/03

--- 02_programming/03_algorithms_day_3/85a6324f3dedf296bf6a4fa08629ebe71779a871/04_square_digits/src/squareDigits.js
+++ 02_programming/03_algorithms_day_3/85a6324f3dedf296bf6a4fa08629ebe71779a871/04_square_digits/src/squareDigits.js
@@ -2,19 +2,20 @@ function squareDigits(x){
   // Code here
 
   if(typeof x === "number"){
-    const squareDigits = x => {
-      const digits = num.toString().split('')
+      const digits = x.toString().split('')
 
       const squaredDigits = digits.map(y => y * y)
 
       const squaredNumber = squaredDigits.join('')
 
       return +squaredNumber
-    }
+    
   } else {
-    throw "paramater is not a number"
+    throw "paramater is not a patate number"
   };
 }
+
+console.log(squareDigits(369))
 
 // Leave the line below for tests to work
 module.exports = squareDigits;
