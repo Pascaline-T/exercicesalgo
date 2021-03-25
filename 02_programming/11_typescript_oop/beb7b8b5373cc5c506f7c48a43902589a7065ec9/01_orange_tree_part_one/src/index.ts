0ea73f3eea3937da0628ea5ed98cb6f9e468bef6
Pascaline_T <pascaline.thuilliez21@gmail.com>
2021-03-25 16:55:06 +0100

    exo du 25/03

---
+++ 02_programming/11_typescript_oop/beb7b8b5373cc5c506f7c48a43902589a7065ec9/01_orange_tree_part_one/src/index.ts
@@ -0,0 +1,36 @@
+// Code the class here.
+abstract class Tree {
+    age: number;
+    height: number;
+    alive: boolean = true;
+
+    constructor(age : number) {
+        this.age = age;
+
+        if(this.age > 20){
+            this.height = (10 * 11 ) + (9 * 25)
+        } 
+        else if(this.age >= 10 && this.age <= 20) {
+            this.height = (9 * 25) + ((this.age - 9) * 10)
+        }
+        else {
+            this.height = this.age * 25
+        }
+    }
+
+    abstract isAlive(): boolean;
+
+    abstract ageOneYear(): void;
+ 
+    seed(): void {
+        this.age = 0;
+        this.height = 0;
+        this.alive = true;
+     }
+
+}
+
+
+
+// Leave the line below for tests to work properly.
+export { Tree };
